a6e66fe50763ba1ce71e4bab9092c3d981bff5cd
Sheikh sadi <sheikhsadi1620@gmail.com>
2023-01-12 00:00:52 +0600

    blacklisted company name added

--- Frontend/DarkModeWithTailwind.js
+++ Frontend/DarkModeWithTailwind.js
@@ -28,17 +28,17 @@ code:
 import { useState, useEffect } from "react";
 
 export default function useDarkSide() {
-	const [theme, setTheme] = useState(localStorage.theme);
-	const colorTheme = theme === "dark" ? "light" : "dark";
+     const [theme, setTheme] = useState(localStorage.theme);
+     const colorTheme = theme === "dark" ? "light" : "dark";
 
-	useEffect(() => {
-		const root = window.document.documentElement;
-		root.classList.remove(colorTheme);
-		root.classList.add(theme);
-		localStorage.setItem('theme', theme);
-	}, [theme, colorTheme]);
+     useEffect(() => {
+          const root = window.document.documentElement;
+          root.classList.remove(colorTheme);
+          root.classList.add(theme);
+          localStorage.setItem('theme', theme);
+     }, [theme, colorTheme]);
 
-	return [colorTheme, setTheme]
+     return [colorTheme, setTheme]
 }
 
  */
@@ -53,26 +53,26 @@ import { DarkModeSwitch } from "react-toggle-dark-mode";
 import useDarkSide from "../hooks/useDarkSide";
 
 export default function Switcher() {
-	const [colorTheme, setTheme] = useDarkSide();
-	const [darkSide, setDarkSide] = useState(
-		colorTheme === "light" ? true : false
-	);
+     const [colorTheme, setTheme] = useDarkSide();
+     const [darkSide, setDarkSide] = useState(
+          colorTheme === "light" ? true : false
+     );
 
-	const toggleDarkMode = (checked) => {
-		setTheme(colorTheme);
-		setDarkSide(checked);
-	};
+     const toggleDarkMode = (checked) => {
+          setTheme(colorTheme);
+          setDarkSide(checked);
+     };
 
-	return (
-		<>
-			<DarkModeSwitch
-				style={{ marginBottom: "2rem" }}
-				checked={darkSide}
-				onChange={toggleDarkMode}
-				size={30}
-			/>
-		</>
-	);
+     return (
+          <>
+               <DarkModeSwitch
+                    style={{ marginBottom: "2rem" }}
+                    checked={darkSide}
+                    onChange={toggleDarkMode}
+                    size={30}
+               />
+          </>
+     );
 }
 
 */
@@ -86,45 +86,45 @@ import React from "react";
 import Switcher from "./Components/Switcher";
 
 function App() {
-	return (
-		<div>
-			<div style={{ textAlign: "center" }}>
-				<h1 className="text-green text-3xl font-bold">
-					GeeksforGeeks
-				</h1>
-				<h3 className="text-black dark:text-white text-2xl">
-					Adding Dark Mode in ReactJS using Tailwind CSS
-				</h3>
-			</div>
-			<center>
-				<Switcher />
-				<div class="w-56 overflow-hidden bg-white
-				rounded-lg border border-gray-200
-				shadow-md dark:bg-gray-800 dark:border-gray-700">
-					<img
-						class="rounded-t-lg"
-						src=
+     return (
+          <div>
+               <div style={{ textAlign: "center" }}>
+                    <h1 className="text-green text-3xl font-bold">
+                         GeeksforGeeks
+                    </h1>
+                    <h3 className="text-black dark:text-white text-2xl">
+                         Adding Dark Mode in ReactJS using Tailwind CSS
+                    </h3>
+               </div>
+               <center>
+                    <Switcher />
+                    <div class="w-56 overflow-hidden bg-white
+                    rounded-lg border border-gray-200
+                    shadow-md dark:bg-gray-800 dark:border-gray-700">
+                         <img
+                              class="rounded-t-lg"
+                              src=
 "https://media.geeksforgeeks.org/wp-content/uploads/20220221132017/download.png"
-						alt="gfg"
-					/>
-					<div class="p-5">
-						<a href="##">
-							<h5 class="mb-2 text-2xl
-							font-bold tracking-tight
-							text-gray-900 dark:text-white">
-								GeeksforGeeks
-							</h5>
-						</a>
-						<p class="mb-3 font-normal text-gray-700
-							dark:text-gray-400">
-							Best coding website for
-							developers in the world.
-						</p>
-					</div>
-				</div>
-			</center>
-		</div>
-	);
+                              alt="gfg"
+                         />
+                         <div class="p-5">
+                              <a href="##">
+                                   <h5 class="mb-2 text-2xl
+                                   font-bold tracking-tight
+                                   text-gray-900 dark:text-white">
+                                        GeeksforGeeks
+                                   </h5>
+                              </a>
+                              <p class="mb-3 font-normal text-gray-700
+                                   dark:text-gray-400">
+                                   Best coding website for
+                                   developers in the world.
+                              </p>
+                         </div>
+                    </div>
+               </center>
+          </div>
+     );
 }
 
 export default App;
---
+++ Others/BlacklistedCompany.md
@@ -0,0 +1,13 @@
+## some balcklisted company
+* Agile RecruiTech LLP
+* Omikron Technologies Private Limited
+* Yougetplaced
+* DeepThought EduTech
+* Black coffer
+* Jalan technology
+* Girl Power Talk
+* XcitEducation Foundations
+* Xcite org.
+* Payrasoft
+* Suvidha Foundation
+* Apyfiy Lab
